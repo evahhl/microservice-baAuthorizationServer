ddc83d5b81425bcd1815d60995526cb3e19ff6db
evahhl <eh@unipagos.com.mx>
2022-08-04 08:49:03 -0700

    * Adding CORS Configuration

--- admin-resource-server/src/main/java/com/nartan/ba/resource/config/ResourceServerConfig.java
+++ admin-resource-server/src/main/java/com/nartan/ba/resource/config/ResourceServerConfig.java
@@ -1,20 +1,33 @@
 package com.nartan.ba.resource.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {// @formatter:off
-        http
+        http.cors()
+                .and()
                 .authorizeRequests(authz -> authz
                         .antMatchers(HttpMethod.GET, "/**").authenticated()
                         //.antMatchers(HttpMethod.POST, "/foos").hasAuthority("SCOPE_write")
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt());
     }// @formatter:on
+
+    @Bean
+    protected CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
+
 }
